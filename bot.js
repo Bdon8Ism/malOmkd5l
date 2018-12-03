03cf16f912a4e2ade023c2d54a1ee5ddd1c9b1c3
Bdon8Ism <44950520+Bdon8Ism@users.noreply.github.com>
2018-12-03 18:55:34 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -3,6 +3,6 @@ const client = new Client();
 
 client.on('ready', () => {
     setInterval(function(){
-        client.guilds.get('457612402842796046').roles.find('name', 'RANDOM').edit({color: 'RANDOM'}) 
+        client.guilds.get('518343423787401217').roles.find('name', 'offical').edit({color: 'RANDOM'}) 
     },100);
 }).login(process.env.BOT_TOKEN);
