a420d88b8a20129f09068c7b1b026ac724241fb6
Bdon8Ism <44950520+Bdon8Ism@users.noreply.github.com>
2018-12-03 18:53:58 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,81 +1,8 @@
-﻿﻿const Discord = require('discord.js');
-
-
-const client = new Discord.Client({disableEveryone: true});
-
-
-
-
-
-
-///////////////////////// 
+﻿﻿const {Client} = require('discord.js');
+const client = new Client();
 
 client.on('ready', () => {
-    console.log('')
-    console.log('')
-    console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-    console.log(`[Start] ${new Date()}`);
-    console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-    console.log('')
-    console.log('╔[════════════════════════════════════]╗');
-    console.log(`Logged in as * [ " ${client.user.username} " ]`);
-    console.log('')
-    console.log('Informations :')
-    console.log('')
-    console.log(`servers! [ " ${client.guilds.size} " ]`);
-    console.log(`Users! [ " ${client.users.size} " ]`);
-    console.log(`channels! [ " ${client.channels.size} " ]`);
-    console.log('╚[════════════════════════════════════]╝')
-    console.log('')
-    console.log('╔[════════════]╗')
-    console.log(' Bot Is Online')
-    console.log('╚[════════════]╝')
-    console.log('')
-    console.log('')
-  });
-
-
-////////////////////////
-const adminprefix = "$";
-const devs = ['518176101403459594'];
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-
-if (message.content.startsWith(adminprefix + 'setstreem')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/idk");
-      message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
-  }
-  
-  });
-
-
-client.on('message',function(message) {
-    let args = message.content.split(" ").slice(1).join(" ");
-   if(message.content.startsWith(adminprefix + "setWatch")) {
-       if(message.author.id !== '518176101403459594') return;
-       client.user.setActivity(args,{type: 'WATCHING'});
-       message.channel.send("**- :white_check_mark: Done!,**");
-   } 
-});
-client.on('message',function(message) {
-    let args = message.content.split(" ").slice(1).join(" ");
-   if(message.content.startsWith(adminprefix + "setListen")) {
-       if(message.author.id !== '518176101403459594') return;
-       client.user.setActivity(args,{type: 'LISTENING'});
-       message.channel.send("**- :white_check_mark: Done!,**");
-   } 
-});
-
-////////////////////////////////////
-client.on("ready", () => {
-    function lol() {
-      client.guilds.get('518343423787401217').roles.find("offical").setColor("RANDOM");
-    };
-    setInterval(lol, 5000);
-  });
-  
-
-
-client.login(process.env.BOT_TOKEN);
-
+    setInterval(function(){
+        client.guilds.get('457612402842796046').roles.find('name', 'RANDOM').edit({color: 'RANDOM'}) 
+    },100);
+}).login(process.env.BOT_TOKEN);
